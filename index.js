ae7279a5430ee2f5f4046a79919ef3bc6623f9b8
Elijah Samuels <elijahsamuels@gmail.com>
2021-02-28 22:11:26 -0700

    commit tree 3255e416e6cb00deb1f6b8936b87dbaeb767b94b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+
+// let even = arr.filter(n => {
+// 	return n % 2 === 0;
+//   });
+  
+// const drivers = [
+// 	{
+// 	  name: 'Bobby',
+// 	  hometown: 'Pittsburgh' },
+// 	{
+// 	  name: 'Sammy',
+// 	  hometown: 'New York' } ,
+// 	{
+// 	  name: 'Sally',
+// 	  hometown: 'Cleveland' },
+// 	{
+// 	  name: 'Annette',
+// 	  hometown: 'Los Angeles' },
+// 	{
+// 	  name: 'Bobby',
+// 	  hometown: 'Tampa Bay' }
+//   ];
+
+function findMatching(drivers, name) {
+	// return drivers.filter((driver) => {
+		// return driver.toLowerCase() === name.toLowerCase()
+	// })
+	return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+	};
+
+function fuzzyMatch(drivers, name) {
+	return drivers.filter(driver => driver.charAt(0) === name.charAt(0))
+};
+
+function matchName(drivers, name) {
+	return drivers.filter(driver => driver.name === name)
+}
